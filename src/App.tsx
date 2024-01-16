cda44d4225fc1cceb3fd9d27a5c24fa52bc006f1
Mustafa Battaloglu <mbattaloglu@devexperts.com>
2024-01-16 12:25:07 +0300

    Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import { selected$, pokemon$, deck$ } from "./store";
+import { useObservableState } from "observable-hooks";
 
-function App() {
+const Deck: React.FC = () => {
+  const deck = useObservableState(deck$, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Deck</h4>
+      <div>
+        {deck.map((p) => (
+          <div key={p.id} style={{ display: "flex" }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
+              alt={p.name}
+            />
+            <div>
+              <div>{p.name}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+const Search: React.FC = () => {
+  const [search, setSearch] = useState("");
+  const pokemon = useObservableState(pokemon$, []);
+
+  const filteredPokemon = useMemo(() => {
+    return pokemon.filter((p) =>
+      p.name.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [pokemon, search]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div>
+        {filteredPokemon.map((p) => (
+          <div key={p.id}>
+            <input
+              type="checkbox"
+              checked={p.selected}
+              onChange={() => {
+                if (selected$.value.includes(p.id)) {
+                  selected$.next(selected$.value.filter((id) => id !== p.id));
+                } else {
+                  selected$.next([...selected$.value, p.id]);
+                }
+              }}
+            />
+            <strong>{p.name}</strong> - {p.power}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+      }}
+    >
+      <Search />
+      <Deck />
+    </div>
+  );
+};
 
 export default App;
